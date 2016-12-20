ac4233ef7807f152f986547335d5f9e91a9475b3
Mufeez <mufeez.ahmed22@gmail.com>
2016-12-20 22:22:10 +0530

    added a class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Student student1=new Student("asif",1,500);
+            
+            string student_name= student1.name;
+
+            Console.WriteLine(student_name);
         }
+    }
+
+    public class Student
+    {
+        public
+        string  name ;
+        int roll_number;
+
+        int total_marks;
+
+        public Student(string name ,int roll_number,int total_marks)
+        {
+            this.name=name;
+            this.roll_number=roll_number;
+            this.total_marks=total_marks;
+
+        }
+ 
+
     }
 }
