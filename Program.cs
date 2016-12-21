30ca4da8a220f9896a884f4da085324e3949f78b
Mufeez <mufeez.ahmed22@gmail.com>
2016-12-21 07:47:41 +0530

    added Stdents Class

--- Program.cs
+++ Program.cs
@@ -7,30 +7,45 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
 
-            Student student1=new Student("asif",1,500);
+            Student student1=new Student("asif",1);
             
-            string student_name= student1.name;
-
-            Console.WriteLine(student_name);
-        }
+           
+            float percentage=student1.Percentage(80,80,80);
+            student1.grade="A";
+            string grade=student1.grade;
+            Console.WriteLine(grade);
+        
+          }
     }
-
     public class Student
     {
-        public
+        private
         string  name ;
-        int roll_number;
+       private  int roll_number;
 
-        int total_marks;
-
-        public Student(string name ,int roll_number,int total_marks)
+       private  int total_marks;
+public string grade{get;set;}
+        public Student(string name ,int roll_number)
         {
             this.name=name;
             this.roll_number=roll_number;
-            this.total_marks=total_marks;
+            this.total_marks=0;
 
+  
         }
  
+        public float Percentage (int maths ,int english ,int science)
+
+            {
+
+                 this.total_marks = maths+english+science;
+
+                float percentage=((float)total_marks/300)*100;
+                return percentage;
+            }
 
     }
 }
+
+
+
