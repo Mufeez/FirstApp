348ec9898efaa5f9948b8f7235965dd5438b8248
Mufeez <mufeez.ahmed22@gmail.com>
2016-12-21 08:08:01 +0530

    modified score handling

--- Program.cs
+++ Program.cs
@@ -8,12 +8,16 @@ namespace ConsoleApplication
         {
 
             Student student1=new Student("asif",1);
-            
+            student1.english_score=80;
+            student1.urdu_score=90;
+            student1.kannada_score=70;
+            student1.social_science_score=90;
+            student1.science_score=80;
+            student1.mathematics_score=70;
            
-            float percentage=student1.Percentage(80,80,80);
-            student1.grade="A";
-            string grade=student1.grade;
-            Console.WriteLine(grade);
+            float percentage=student1.Percentage();
+           
+            Console.WriteLine(percentage);
         
           }
     }
@@ -23,24 +27,28 @@ namespace ConsoleApplication
         string  name ;
        private  int roll_number;
 
-       private  int total_marks;
-public string grade{get;set;}
+       public int english_score{get;set;}
+       public  int urdu_score{get;set;}
+       public  int kannada_score{get;set;}
+       public  int mathematics_score{get;set;}
+       public  int social_science_score{get;set;}
+       public  int science_score{get;set;}
+       private string grade;
         public Student(string name ,int roll_number)
         {
             this.name=name;
             this.roll_number=roll_number;
-            this.total_marks=0;
-
+            
   
         }
  
-        public float Percentage (int maths ,int english ,int science)
+        public float Percentage ()
 
             {
 
-                 this.total_marks = maths+english+science;
+                int total_marks = this.mathematics_score+this.kannada_score+this.science_score+this.social_science_score+this.urdu_score+this.english_score;
 
-                float percentage=((float)total_marks/300)*100;
+                float percentage=((float)total_marks/600)*100;
                 return percentage;
             }
 
